49d80bf04e735b3dacea3d2416a77c602a5f10e2
Thiago lovatine <thiagolovatine@gmail.com>
2021-09-26 13:24:12 -0300

    Initial Commit

---
+++ Shared/OnDemand_resourcesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnDemand_resourcesApp.swift
+//  Shared
+//
+//  Created by Thiago on 26/09/21.
+//
+
+import SwiftUI
+
+@main
+struct OnDemand_resourcesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
